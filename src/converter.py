88586e316826864150e5278ed393e05e296643dd
Eoin Pinaqui <pinaquie@tcd.ie>
2021-02-16 17:46:46 +0000

    Hello There!

--- src/converter.py
+++ src/converter.py
@@ -7,7 +7,7 @@ import os
 # A function that returns "Hello World". This function is called in start_gui() in gui.py.
 # Change what is returned in this function to edit what is displayed in the "Hello World" GUI.
 def hello_world():
-    return "Hello World"
+    return "Hello There"
 
 
 # A function that returns "In CLI". This function is called in main.py to signify that the GUI has not been started.
