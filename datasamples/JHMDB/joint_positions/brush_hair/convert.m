90fa74d7f841950bcda9aa83a06f49c90fd812cc
neimhin <nrobinso@tcd.ie>
2021-02-25 00:04:46 +0000

    created rough script for converting jhmdb .mat files to csv

--- datasamples/JHMDB/joint_positions/brush_hair/convert.m
+++ datasamples/JHMDB/joint_positions/brush_hair/convert.m
@@ -1,11 +1,15 @@
-CurrentFolder = pwd;
-AllFile=dir(fullfile(CurrentFolder,'F*'));
+
+
+
+CurrentFolder=pwd;
+AllFile=dir('.');
 FolderOnly=AllFile([AllFile.isdir]);
 
 
-for k=1:length(FolderOnly)
-  load(strcat(FolderOnly{k},'/joint_positions.mat'))
-  csvwrite(strcat(FolderOnly{k},'/pos_img.csv'),pos_img)
-  csvwrite(strcat(FolderOnly{k},'/pos_world.csv'),pos_world)
-  csvwrite(strcat(FolderOnly{k},'/scale.csv'),scale)
+for k=3:length(FolderOnly)
+  disp(FolderOnly(k).name);
+  d=load(strcat(FolderOnly(k).name,'/joint_positions.mat'));
+  csvwrite(strcat(FolderOnly(k).name,'/pos_img.csv'),d.pos_img);
+  csvwrite(strcat(FolderOnly(k).name,'/pos_world.csv'),d.pos_world);
+  csvwrite(strcat(FolderOnly(k).name,'/scale.csv'),d.scale);
 end
