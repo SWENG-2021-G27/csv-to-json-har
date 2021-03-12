d9c14d0dd4146490f8ebf9ddc6d9ee9a7b6b57f2
neimhin <nrobinso@tcd.ie>
2021-03-12 14:04:49 +0000

    error colouring for load_configuration.py

--- src/load_configuration.py
+++ src/load_configuration.py
@@ -9,6 +9,8 @@ configuration = {
 }
 
 
+# ANSI color codes can be seen here: https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
+
 def load_config(path_to_config, encoding="utf-8"):
   print("\033[0;37;40mLoading configuration from: " + path_to_config)
   f = open(path_to_config, "r", encoding=encoding)
@@ -24,13 +26,12 @@ def load_config(path_to_config, encoding="utf-8"):
       char = line[i]
 
     if(configuration.get(key)):
-      print( key + " is a valid configuration variable")
       configuration[key] = line[len(key):len(line)-1]
-      print( "configuration[" + key + "] has been updated to " + configuration[key])
+      print( "\033[0;32mconfiguration[" + key + "] has been updated to " + configuration[key] + " \u2713")
     else :
       print("\033[1;31;44mERROR " + key + " is not a recognised configuration variable\033[0;37;40m")      
-    print(line[:len(line)-1] + " \u2713",)
     line = f.readline()
+
   f.close()
 
 
