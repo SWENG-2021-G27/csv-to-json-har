cfa8b651a7b28bb23a2e7d0106b3c705ae9c507c
neimhin <nrobinso@tcd.ie>
2021-03-12 14:14:20 +0000

    added silent option to load_configuration.py

--- src/load_configuration.py
+++ src/load_configuration.py
@@ -11,8 +11,9 @@ configuration = {
 
 # ANSI color codes can be seen here: https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
 
-def load_config(path_to_config, encoding="utf-8"):
-  print("\033[0;37;40mLoading configuration from: " + path_to_config)
+def load_config(path_to_config, encoding="utf-8",silent=False):
+  if(not silent):
+    print("\033[0;37;40mLoading configuration from: " + path_to_config)
   f = open(path_to_config, "r", encoding=encoding)
   
   line = f.readline()
@@ -27,13 +28,15 @@ def load_config(path_to_config, encoding="utf-8"):
 
     if(configuration.get(key)):
       configuration[key] = line[len(key):len(line)-1]
-      print( "\033[0;32mconfiguration[" + key + "] has been updated to " + configuration[key] + " \u2713")
+      if(not silent):
+        print( "\033[0;32mconfiguration['" + key + "'] has been updated to " + type(configuration[key]).__name__ + ": '" +  configuration[key] + "' \u2713")
     else :
-      print("\033[1;31;44mERROR " + key + " is not a recognised configuration variable\033[0;37;40m")      
+      if(not silent):
+        print("\033[1;31;44mERROR " + key + " is not a recognised configuration variable\033[0;37;40m")      
     line = f.readline()
 
   f.close()
 
 
 load_config("config")
-  
+load_config("config",silent=True)
