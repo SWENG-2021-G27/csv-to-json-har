3038b563323e2024f4a37f04493391bd42888c4d
Eoin Pinaqui <pinaquie@tcd.ie>
2021-03-17 16:27:11 +0000

    Made conversion correct

--- src/converter.py
+++ src/converter.py
@@ -35,9 +35,10 @@ def convert_vertical(filename, output, c):
     # Open the output file in write mode and write the header fields of the json
     out = open(output, "w")
     out.write("{\"d\":" + str(c.config["Device"]) + ","
-              + "\"g\":" + str(c.config["Ground"]) + ","
-              + "\"o\":" + str(c.config["Offset"]) + ","
-              + "\"t\":{\"Items\":[")
+              + "\"g\":\"(" + str(c.config["Ground"][0]) + "," + str(c.config["Ground"][1]) + "," + str(
+        c.config["Ground"][2]) + "," + str(c.config["Ground"][3]) + ")\","
+              + "\"o\":\"" + str(c.config["Offset"]) + "\","
+              + "\"t\":\"{\\\"Items\\\":[")
 
     # Read every row of the csv and process the data accordingly
     with open(filename) as f:
@@ -45,35 +46,32 @@ def convert_vertical(filename, output, c):
         frame = 0
         for row in csv.reader(f):
             if row_idx > c.config["StartRow"]:
-
                 # Write the frame field
                 if frame > 0:
                     out.write(",")
-                out.write("{\"f\":")
-                if c.config["Frames"]["FramesInfo"]:
-                    out.write(str(row[c.config["Frames"]["FrameIdx"]]))
-                else:
-                    out.write(str(frame * 33))
+                out.write("{\\\"f\\\":")
+                out.write(str((frame + 1) * 33))
                 out.write(",")
 
                 # Write the joint information appropriately
-                out.write("\"b\":{\"i\":" + str(frame + 1) + ",")
-                out.write("\"j\":[")
+                out.write("\\\"b\\\":{\\\"i\\\":" + str(frame + 1) + ",")
+                out.write("\\\"j\\\":[")
 
                 for joint in joint_order:
-                    out.write("{\"s\":2,")
-                    out.write("\"p\":" + str([c.config["Joints"][joint]["x"], c.config["Joints"][joint]["y"],
-                                              c.config["Joints"][joint]["z"]]) + ",")
-                    out.write("\"q\":[0,0],")
-                    out.write("\"o\":[0,0,0,0]}")
+                    out.write("{\\\"s\\\":2,")
+                    out.write("\\\"p\\\":\\\"(" + str(row[c.config["Joints"][joint]["x"]]).strip() + ","
+                              + str(row[c.config["Joints"][joint]["y"]]).strip() + ","
+                              + str(float(row[c.config["Joints"][joint]["z"]])+200).strip() + ")\\\",")
+                    out.write("\\\"q\\\":\\\"(0,0)\\\",")
+                    out.write("\\\"o\\\":\\\"(0,0,0,0)\\\"}")
                     if joint != joint_order[len(joint_order) - 1]:
                         out.write(",")
 
                 out.write("],")
-                out.write("\"r\":0,")
-                out.write("\"l\":0,")
-                out.write("\"_negativeGroundOffset\":0.0,")
-                out.write("\"_previousNegativeGroundOffset\":0.0")
+                out.write("\\\"r\\\":0,")
+                out.write("\\\"l\\\":0,")
+                out.write("\\\"_negativeGroundOffset\\\":0.0,")
+                out.write("\\\"_previousNegativeGroundOffset\\\":0.0")
                 out.write("}")
 
                 frame = frame + 1
@@ -81,4 +79,4 @@ def convert_vertical(filename, output, c):
 
             row_idx = row_idx + 1
 
-        out.write("]}}")
+        out.write("]}\"}")
