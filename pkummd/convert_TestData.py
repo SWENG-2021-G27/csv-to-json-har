eb8ba5df4db0c50cb5b5be48bbb82ca8c79732ac
neimhin <nrobinso@tcd.ie>
2021-03-22 18:23:37 +0000

    conversion attempt

--- pkummd/convert_TestData.py
+++ pkummd/convert_TestData.py
@@ -7,4 +7,11 @@ sep = os.path.sep
 config_file = '..' + sep + 'pkummd' + sep + 'config.json'
 input_file = '..' + sep + 'pkummd' + sep + 'TestData' + sep + '0002-L.txt'
 output_file = '..' + sep + 'pkummd' + sep + 'TestData' + sep + '0002-L.json'
+
+print(os.getcwd())
+
+with open(input_file, "r") as f:
+  line = f.readline()
+  print(line)
+
 exit_code = os.system('python main.py ' + config_file + ' ' + input_file + ' ' + output_file)
