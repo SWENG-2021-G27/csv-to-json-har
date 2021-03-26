e305366a2d93a8f70c8e5d97bc3837f8d7a78ce4
neimhin <nrobinso@tcd.ie>
2021-03-26 16:59:23 +0000

    trying M at different conversion factors

--- helper_scripts/pkummd/convert_TestData.py
+++ helper_scripts/pkummd/convert_TestData.py
@@ -3,8 +3,8 @@ import os
 sep = os.path.sep
 
 config_file = os.path.abspath('config.json')
-input_file = os.path.abspath('TestData' + sep + '0002-L.txt')
-output_file = os.path.abspath('TestOutput' + sep + '0002-L.json')
+input_file = os.path.abspath('TestData' + sep + '0002-M.txt')
+output_file = os.path.abspath('TestOutput' + sep + '0002-M.json')
 
 os.chdir('..' + sep + '..')
 os.chdir('src')
