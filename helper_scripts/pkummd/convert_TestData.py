45cab50a26fe89cfe5796bf2a64577bd5b94dfef
neimhin <nrobinso@tcd.ie>
2021-04-01 15:50:11 +0100

    still trying to get pkummd working

--- helper_scripts/pkummd/convert_TestData.py
+++ helper_scripts/pkummd/convert_TestData.py
@@ -7,6 +7,7 @@ input_file = os.path.abspath('TestData' + sep + '0222-M.txt')
 output_file = os.path.abspath('TestOutput' + sep + 'non_verified_0222-M.json')
 
 os.chdir('..' + sep + '..')
+os.chdir('src')
 
 exit_code = os.system('python main.py ' + config_file + ' ' + input_file + ' ' + output_file)
 
