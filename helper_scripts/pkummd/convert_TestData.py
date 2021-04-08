c1bf911c57974ef3eedf58f6a01fa66cef04a553
Neimhin <nrobinso@tcd.ie>
2021-04-08 18:39:49 +0100

    pkummd finally working ;P, very janky looking though

--- helper_scripts/pkummd/convert_TestData.py
+++ helper_scripts/pkummd/convert_TestData.py
@@ -1,4 +1,7 @@
 import os
+import colorama
+
+colorama.init()
 
 sep = os.path.sep
 
@@ -11,5 +14,11 @@ os.chdir('src')
 
 exit_code = os.system('python main.py -c ' + config_file + ' -i ' + input_folder + ' -o ' + output_folder)
 
+if(exit_code ==  0):
+  print( colorama.Fore.GREEN )
+else:
+  print( colorama.Fore.RED )
 
-print('Finished convert_TestData.py with exit code: ' + str(exit_code))
+print('\tFinished convert_TestData.py with exit code: ' + str(exit_code))
+
+print( colorama.Style.RESET_ALL )
