c9271b370a033a0f259a11802ee656a1d51ccb94
Neimhin <nrobinso@tcd.ie>
2021-04-08 17:28:23 +0100

    minor tweaks to pkummd config and scripts

--- helper_scripts/pkummd/convert_TestData.py
+++ helper_scripts/pkummd/convert_TestData.py
@@ -3,13 +3,13 @@ import os
 sep = os.path.sep
 
 config_file = os.path.abspath('config.json')
-input_file = os.path.abspath('TestData' + sep + '0222-M.txt')
-output_file = os.path.abspath('TestOutput' + sep + 'non_verified_0222-M.json')
+input_folder = os.path.abspath('TestData')
+output_folder = os.path.abspath('TestOutput')
 
 os.chdir('..' + sep + '..')
 os.chdir('src')
 
-exit_code = os.system('python main.py ' + config_file + ' ' + input_file + ' ' + output_file)
+exit_code = os.system('python main.py -c ' + config_file + ' -i ' + input_folder + ' -o ' + output_folder)
 
 
 print('Finished convert_TestData.py with exit code: ' + str(exit_code))
