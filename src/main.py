ef4a520f2ca5ed82f2dda8cd9ecce849cce003fe
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-16 23:33:27 +0100

    Fixed some small typos

--- src/main.py
+++ src/main.py
@@ -7,15 +7,18 @@ import sys
 import os
 import re
 import colorama
+
 # This makes the coloring cross platform
 colorama.init()
 
 # Use "<dir>" + sep + "<dir>"  instead of <dir>\\<dir> or <dir>/<dir> for building paths to make it cross-platform
 sep = os.path.sep
 
-# WARNING the current working directory is determined by where the program is called from not where the program is. If the user is not in the src directory when the program is called, the relative paths will not work
+# WARNING the current working directory is determined by where the program is called from not where the program is.
+# If the user is not in the src directory when the program is called, the relative paths will not work
 # We can use the first command line argument to change to src see if __name__ ==... section below
-# neimhin: I think this should be changed such that for each folder in RawDatasets we check if there's a config.json and use that for that folder
+# neimhin: I think this should be changed such that for each folder in RawDatasets we check if
+# there's a config.json and use that for that folder
 
 # In case we need to know where the user called the program from we can use this variable
 user_working_directory = os.getcwd()
@@ -28,14 +31,17 @@ def warn(warning):
 
 def error(error_msg):
     # error in red
-    print("\n\t" + colorame.Fore.RED + "[ERROR]: " + colorama.Style.RESET_ALL + error_msg + "\n")
+    print("\n\t" + colorama.Fore.RED + "[ERROR]: " + colorama.Style.RESET_ALL + error_msg + "\n")
     sys.exit(-1)
+
 
 def nice_msg(msg):
     print("\n\t" + colorama.Fore.GREEN + msg + colorama.Style.RESET_ALL + "\n")
 
+
 def blue(msg):
-    return colorama.Fore.BLUE + msg + colorama.Style.RESET_ALL 
+    return colorama.Fore.BLUE + msg + colorama.Style.RESET_ALL
+
 
 default_options = {
     "config_file_path": ".." + sep + "RawDatasets" + sep + "PKUMMD" + sep + "config.json",
@@ -58,13 +64,13 @@ extra_options = {
 }
 
 flags = {
-  "-c": "config_file_path",
-  "--config": "config_file_path",
-  "-i": "input_path",
-  "--input": "input_path",
-  "-o": "output_folder",
-  "--output": "output_folder",
- }
+    "-c": "config_file_path",
+    "--config": "config_file_path",
+    "-i": "input_path",
+    "--input": "input_path",
+    "-o": "output_folder",
+    "--output": "output_folder",
+}
 
 
 # This function will either launch the GUI or operate as a CLI.
@@ -74,26 +80,26 @@ def main():
     command_line_arguments = sys.argv
 
     global options, extra_options
-    
-    nice_msg("Parsing the following arguments: " +  str(command_line_arguments[1:]))
+
+    nice_msg("Parsing the following arguments: " + str(command_line_arguments[1:]))
     # If the -gui field is present in the command line arguments, open the GUI
     if "--gui" in command_line_arguments or '-g' in command_line_arguments:
         extra_options["gui"] = False
-        if (len(command_line_arguments) > 2):
+        if len(command_line_arguments) > 2:
             print("Starting GUI and ignoring other command line arguments")
         start_gui()
-        # The GUI and the CLI are not setup to work in tandem, thus for each instance of the program the user can only start one of them
-        # Therefore end the main function if start the gui
+        # The GUI and the CLI are not setup to work in tandem, thus for each instance of the program the user can
+        # only start one of them. Therefore end the main function if start the gui
         return
 
     # If the --input field is present in the command line arguments, update the input directory
     overwriting = None
-    for opt in ['-c', '--config', '-i', '--input','-o', '--output']:
+    for opt in ['-c', '--config', '-i', '--input', '-o', '--output']:
         if opt in command_line_arguments:
             idx = command_line_arguments.index(opt)
             if idx >= len(command_line_arguments) - 1:
-                error(  blue(opt) + " field incorrectly specified." +
-                        " The " + blue(opt) + " option takes exactly one parameter. Zero given.")
+                error(blue(opt) + " field incorrectly specified." +
+                      " The " + blue(opt) + " option takes exactly one parameter. Zero given.")
                 return
             value = command_line_arguments[idx + 1]
             # delete opt
@@ -101,9 +107,9 @@ def main():
             # delete value
             command_line_arguments.pop(idx)
             option_to_set = flags[opt]
-            if(option[option_to_set] != None):
-              warn( "The " + blue(option_to_set) + " is being overwitten.\n\t" +
-                    blue(option_to_set) + " is now set to " + blue(value) + " (argument of " + blue(opt) + ")")
+            if options[option_to_set] is not None:
+                warn("The " + blue(option_to_set) + " is being overwritten.\n\t" +
+                     blue(option_to_set) + " is now set to " + blue(value) + " (argument of " + blue(opt) + ")")
             options[option_to_set] = value
 
     # We don't need the first argument (i.e. the path to main.py)
@@ -116,77 +122,82 @@ def main():
     idx = 0
     while idx < len(command_line_arguments):
         arg = command_line_arguments[idx]
-        if (re.match(single_char_switch, arg)):
+        if re.match(single_char_switch, arg):
             warn(arg + " is not a recognised switch and is being ignored")
             command_line_arguments.pop(idx)
-        elif (re.match(too_many_chars, arg)):
-            warn(arg + " is not a recognised switch and is being ignored.\n\tNOTE: multi character switches begin with two dashes, e.g. --config, wheras single character switches begin with one dash, e.g -c")
+        elif re.match(too_many_chars, arg):
+            warn(
+                arg + " is not a recognised switch and is being ignored.\n\tNOTE: multi character switches begin with "
+                      "two dashes, e.g. --config, wheras single character switches begin with one dash, e.g -c")
             command_line_arguments.pop(idx)
-        elif (re.match(multi_char_switch, arg)):
+        elif re.match(multi_char_switch, arg):
             warn(arg + " is not a recognised switch and is being ignored.")
             command_line_arguments.pop(idx)
         else:
             idx += 1
 
-    
     # Consume remaining arguments
-    if (len(command_line_arguments) > 0):
-      # Assign remaining options in default order
-      for opt in options.keys():
-        if(len(command_line_arguments) == 0):
-          break
-        if options[opt] == None:
-          options[opt] = command_line_arguments[0]
-          command_line_arguments.pop(0)
-    
+    if len(command_line_arguments) > 0:
+        # Assign remaining options in default order
+        for opt in options.keys():
+            if len(command_line_arguments) == 0:
+                break
+            if options[opt] is None:
+                options[opt] = command_line_arguments[0]
+                command_line_arguments.pop(0)
+
     # Use default_options for any options that are still None
     for opt in options.keys():
-      if options[opt] == None:
-        warn("Using default " + opt + ": " + default_options[opt] + "\n\tUse " + blue(option_flag[opt] + " <" + opt +">") + " to set the " + opt +" manually.")
-        options[opt] = default_options[opt]  
-      
-    if (len(command_line_arguments) > 0):
-        # Show command line arguments that weren't consumed. The first argument should not have been consumed. It is the name of the program that was called.
+        if options[opt] is None:
+            warn("Using default " + opt + ": " + default_options[opt] + "\n\tUse " + blue(
+                option_flag[opt] + " <" + opt + ">") + " to set the " + opt + " manually.")
+            options[opt] = default_options[opt]
+
+    if len(command_line_arguments) > 0:
+        # Show command line arguments that weren't consumed.
+        # The first argument should not have been consumed. It is the name of the program that was called.
         warn("The following command line arguments were not matched in any pattern and were not loaded: ")
         print(command_line_arguments)
 
     # Are we converting a whole folder or just a single file?
-    if(os.path.isfile(options["input_path"])):
-      extra_options["single_file"] = True
-    
+    if os.path.isfile(options["input_path"]):
+        extra_options["single_file"] = True
+
     try:
-      x = Configuration(options["config_file_path"])
+        x = Configuration(options["config_file_path"])
     except OSError:
-      error("OSError raised while loading config from " + blue(options["config_file"]))
+        error("OSError raised while loading config from " + blue(options["config_file"]))
     except ValueError:
-      error("ValueError raised while loading config from " + blue(options["config_file"]))
+        error("ValueError raised while loading config from " + blue(options["config_file"]))
     except:
-      error("An unknown error has occurred while loading config from " + blue(options["config_file"]))
-      
-    if(extra_options["single_file"]):
-      #TODO NYI
-      return
+        error("An unknown error has occurred while loading config from " + blue(options["config_file"]))
+
+    if (extra_options["single_file"]):
+        # TODO NYI
+        return
     # Convert all csv files in the input directory
     if not os.path.exists(options["output_folder"]):
-      try:
-        os.makedirs(options["output_folder"])
-      except OSError:
-        error("Failed to create output directory " + options["output_folder"])
-      except:
-        error("An unknown fatal errr has occurred while trying to creat the output folder " + blue(options["output_foler"]))
+        try:
+            os.makedirs(options["output_folder"])
+        except OSError:
+            error("Failed to create output directory " + options["output_folder"])
+        except:
+            error("An unknown fatal errr has occurred while trying to creat the output folder " + blue(
+                options["output_folder"]))
     for filename in os.listdir(options["input_path"]):
-            if filename.endswith(x.config["FileExtension"]):
-                base = os.path.splitext(filename)[0]
-                if x.config["Structure"] == "Vertical":
-                    convert_vertical(options["input_path"] + sep + filename,
-                                     options["output_folder"] + sep + base + ".json", x)
-                elif x.config["Structure"] == "NTU":
-                    convert_ntu(options["input_path"] + sep + filename,
-                                options["output_folder"] + sep + base + ".json", x)
+        if filename.endswith(x.config["FileExtension"]):
+            base = os.path.splitext(filename)[0]
+            if x.config["Structure"] == "Vertical":
+                convert_vertical(options["input_path"] + sep + filename,
+                                 options["output_folder"] + sep + base + ".json", x)
+            elif x.config["Structure"] == "NTU":
+                convert_ntu(options["input_path"] + sep + filename,
+                            options["output_folder"] + sep + base + ".json", x)
 
 
 if __name__ == "__main__":
-    # Here's how we could change to the src directory if the user is calling the program from another directory. At the moment we're not set up to handle this.
+    # Here's how we could change to the src directory if the user is calling the program from another directory.
+    # At the moment we're not set up to handle this.
     """
     path_to_src = sys.argv[0]
     # Split the path by the systems path seperator
