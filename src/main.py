eb1a5639f5a5ca33aa2f6592a545dd7167542482
Neimhin <nrobinso@tcd.ie>
2021-04-17 14:25:49 +0100

    first implementation of recursive conversion

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # main.py
-
-from gui import *
-from converter import *
-from configuration import *
+import gui
+import converter
+import configuration
 import sys
 import os
+import pathlib
+import ntpath
 import re
 import colorama
 
@@ -46,7 +47,7 @@ def blue(msg):
 default_options = {
     "config_file_path": ".." + sep + "RawDatasets" + sep + "PKUMMD" + sep + "config.json",
     "input_path": ".." + sep + "RawDatasets" + sep + "PKUMMD",
-    "output_folder": ".." + sep + "ConvertedJsonOutput" + sep + "PKUMMD",
+    "output_folder": os.path.join(os.pardir, "ConvertedJsonOutput", "PKUMMD"),
 }
 option_flag = {
     "config_file_path": "--config",
@@ -82,19 +83,18 @@ def main():
     global options, extra_options
 
     nice_msg("Parsing the following arguments: " + str(command_line_arguments[1:]))
-    # If the -gui field is present in the command line arguments, open the GUI
+    # If the --gui field is present in the command line arguments, open the GUI
     if "--gui" in command_line_arguments or '-g' in command_line_arguments:
-        extra_options["gui"] = False
         if len(command_line_arguments) > 2:
             print("Starting GUI and ignoring other command line arguments")
-        start_gui()
+        gui.start_gui()
         # The GUI and the CLI are not setup to work in tandem, thus for each instance of the program the user can
         # only start one of them. Therefore end the main function if start the gui
         return
 
     # If the --input field is present in the command line arguments, update the input directory
     overwriting = None
-    for opt in ['-c', '--config', '-i', '--input', '-o', '--output']:
+    for opt in flags.keys():
         if opt in command_line_arguments:
             idx = command_line_arguments.index(opt)
             if idx >= len(command_line_arguments) - 1:
@@ -162,37 +162,78 @@ def main():
     # Are we converting a whole folder or just a single file?
     if os.path.isfile(options["input_path"]):
         extra_options["single_file"] = True
+    elif not os.path.isdir(options["input_path"]):
+        error(blue(options["input_path"]) + " is neither a file nor a folder. Aborting.")
+        sys.exit(-1)
+
 
     try:
-        x = Configuration(options["config_file_path"])
-    except OSError:
-        error("OSError raised while loading config from " + blue(options["config_file"]))
-    except ValueError:
-        error("ValueError raised while loading config from " + blue(options["config_file"]))
-    except:
-        error("An unknown error has occurred while loading config from " + blue(options["config_file"]))
+      x = configuration.Configuration(options["config_file_path"])
+    except OSError as e:
+        error(str(e) + ": OSError raised while loading config from " + blue(options["config_file_path"]))
+    except ValueError as e:
+        error(str(e) + ": ValueError raised while loading config from " + blue(options["config_file_path"]))
+    except Exception as e:
+        error(str(e) + ": An unknown error has occurred while loading config from " + blue(options["config_file_path"]))
 
-    if (extra_options["single_file"]):
-        # TODO NYI
-        return
-    # Convert all csv files in the input directory
+    if os.path.isfile(options["output_folder"]):
+        error(blue(options["output_folder"]) + " is a file. " + blue("output_folder") + " (given by " + 
+              blue("-o") + " or " + blue("--output") + " must be a folder.")
     if not os.path.exists(options["output_folder"]):
         try:
             os.makedirs(options["output_folder"])
-        except OSError:
-            error("Failed to create output directory " + options["output_folder"])
-        except:
-            error("An unknown fatal errr has occurred while trying to creat the output folder " + blue(
+        except OSError as e:
+            error(str(e) + " Failed to create output directory " + options["output_folder"])
+        except Exception as e:
+            error(str(e) + " An unknown fatal error has occurred while trying to creat the output folder " + blue(
                 options["output_folder"]))
-    for filename in os.listdir(options["input_path"]):
-        if filename.endswith(x.config["FileExtension"]):
-            base = os.path.splitext(filename)[0]
-            if x.config["Structure"] == "Vertical":
-                convert_vertical(options["input_path"] + sep + filename,
-                                 options["output_folder"] + sep + base + ".json", x)
-            elif x.config["Structure"] == "NTU":
-                convert_ntu(options["input_path"] + sep + filename,
-                            options["output_folder"] + sep + base + ".json", x)
+    
+    options["output_folder"] = os.path.abspath(options["output_folder"])
+
+    # Convert all csv files in the input directory
+    if x.config["Structure"] == "Vertical":
+      os.chdir(options["input_path"]) 
+      recursive_convert_vertical(".",
+                                 options["output_folder"], x)
+    elif x.config["Structure"] == "NTU":
+      recursive_convert_ntu(options["input_path"],
+                            os.path.abspath(options["output_folder"]), x)
+    else:
+      error("No Structure provided in config.json. Possible Structure values are: \"Vertical\", \"NTU\"")
+
+
+def recursive_convert_vertical(input, output, config):
+  for file_or_dir in os.listdir('.'):
+    if os.path.isdir(file_or_dir):
+      nice_msg("Descending into " + blue(os.path.abspath(input)))
+      os.chdir(os.path.abspath(file_or_dir))
+      print(os.path.join(output, file_or_dir))
+      recursive_convert_vertical( os.getcwd(),
+                                  os.path.join(output, file_or_dir), config)
+      nice_msg("Ascending back to " + blue(os.path.abspath(os.pardir)))
+      os.chdir(os.pardir)
+    elif os.path.isfile(file_or_dir) and file_or_dir.endswith(config.config["FileExtension"]):
+      base = os.path.splitext(file_or_dir)[0]
+      nice_msg("Converting: " + os.path.join(input, file_or_dir) + blue(" into ") + "\n\t" + os.path.join(output, base + ".json"))
+      if not os.path.exists(output):
+        os.makedirs(output)
+      converter.convert_vertical(os.path.join(input,file_or_dir),
+                       os.path.join(output, base + ".json"), config)
+
+
+
+def recursive_convert_ntu(input, output, config):
+  for file_or_dir in os.listdir(input):
+    if os.path.isdir(file_or_dir):
+      os.chdir(input)
+      print(os.path.join(output,file_or_dir))
+      recursive_convert_ntu(os.getcwd(),
+                            os.path.join(output, file_or_dir), config)
+      os.chdir(os.pardir)
+    elif os.path.isfile(file_or_dir) and file_or_dir.endswith(config.config["FileExtension"]):
+      base = os.path.splitext(file_or_dir)[0] 
+      converter.convert_ntu(  os.path.join(input, file_or_dir),
+                    os.path.joint(output, base + ".json"), config)
 
 
 if __name__ == "__main__":
@@ -208,4 +249,4 @@ if __name__ == "__main__":
     print("Current working directory is now: " + os.getcwd())
     """
     main()
-    print(colorama.Fore.GREEN + "\tFinished!" + colorama.Style.RESET_ALL)
+    print(colorama.Fore.GREEN + "\n\tFinished!" + colorama.Style.RESET_ALL)
