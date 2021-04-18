72fb0bcbe1622745505e723aad219063342a3213
Mark Lysaght <57681015+marklysaght@users.noreply.github.com>
2021-04-18 22:56:11 +0100

    script to download berkeley dataset from google drive

--- helper_scripts/BerkeleyMHAD/download_mhad.py
+++ helper_scripts/BerkeleyMHAD/download_mhad.py
@@ -1,37 +1,39 @@
-#taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
+# taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
 import requests
 import os
 import sys
 from tqdm import tqdm
 
+
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
-    
+
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={'id': id}, stream=True)
     token = get_confirm_token(response)
 
-    total_size_in_bytes = int(response.headers.get('content-length',0)) or None
+    total_size_in_bytes = int(
+        response.headers.get('content-length', 0)) or None
 
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)      
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
 
-    
     CHUNK_SIZE = 32768
-    
+
     with open(destination, "wb") as f:
-      for chunk in response.iter_content(CHUNK_SIZE):
-        progress_bar.update(len(chunk))
-        if chunk: # filter out keep-alive new chunks
-          f.write(chunk)
-    
+        for chunk in response.iter_content(CHUNK_SIZE):
+            progress_bar.update(len(chunk))
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-      print("ERROR, something went wrong")
+        print("ERROR, something went wrong")
     progress_bar.close()
+
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
@@ -40,19 +42,21 @@ def get_confirm_token(response):
 
     return None
 
+
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
 
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
 
-file_id = '0B20a4UzO-OyMQW9wVHhlVmZBR0k'
+file_id = '1uomG5szmcaTsUy6w7CPTLsgBa2by1h3s'
 sep = os.path.sep
-destination_folder = os.path.abspath('..' + sep + '..' + sep + 'RawDatasets' + sep + 'MHAD' )
+destination_folder = os.path.abspath(
+    '..' + sep + '..' + sep + 'RawDatasets' + sep + 'BerkeleyMHAD')
 os.mkdir(destination_folder)
-destination = destination_folder + sep + 'Skeleton.7z'
-print('Downloading PKUMMD zip to ' + destination)
+destination = destination_folder + sep + 'BerkeleyMHAD.7z'
+print('Downloading BerkeleyMHAD zip to ' + destination)
 download_file_from_google_drive(file_id, destination)
