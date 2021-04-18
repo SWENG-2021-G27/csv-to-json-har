8389757aad36739554abb7d000a90babff59cf9a
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-18 23:21:00 +0100

    Added bvh_to_csv.py for Berkeley

---
+++ helper_scripts/BerkeleyMHAD/bvh_to_csv.py
@@ -0,0 +1,7 @@
+import os
+
+for subdir, dirs, files in os.walk("../../RawDatasets/BerkeleyMHAD/Mocap"):
+    for file in os.listdir(subdir):
+        if file.endswith(".bvh"):
+            print(file)
+            os.system("bvh2csv " + subdir + "\\" + file + " --position --out ../../RawDatasets/BerkeleyMHAD-CSV")
