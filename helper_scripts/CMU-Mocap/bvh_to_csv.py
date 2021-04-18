983fcfe3dafa327d6fde4bfe1292f99221caf5e9
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-18 21:23:49 +0100

    Added README to CMU and SFU

--- helper_scripts/CMU-Mocap/bvh_to_csv.py
+++ helper_scripts/CMU-Mocap/bvh_to_csv.py
@@ -1,6 +1,6 @@
 import os
 
-for subdir, dirs, files in os.walk("./CMU-Data"):
+for subdir, dirs, files in os.walk("./CMU-Data-BVH"):
     for file in os.listdir(subdir):
         if file.endswith(".bvh"):
             os.system("bvh2csv " + subdir + "\\" + file + " --position --out ./CMU-Data-CSV")
