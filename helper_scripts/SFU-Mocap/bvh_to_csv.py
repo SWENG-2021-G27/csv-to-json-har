05034f72059724884b71bcfb5e229245f1499627
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-18 23:30:46 +0100

    Printing file on bvh conversion SFU

--- helper_scripts/SFU-Mocap/bvh_to_csv.py
+++ helper_scripts/SFU-Mocap/bvh_to_csv.py
@@ -4,4 +4,5 @@ print("This will take a while")
 for subdir, dirs, files in os.walk("./SFU-Data-BVH"):
     for file in os.listdir(subdir):
         if file.endswith(".bvh"):
+            print(file)
             os.system("bvh2csv " + subdir + "\\" + file + " --position --out ../../RawDatasets/SFU-Data-CSV")
