194998cf584956b2a37197e257c99366429b63d3
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-19 16:48:44 +0100

    Added TestData

--- helper_scripts/MSRC-12-Kinect-Gesture/convert_TestData.py
+++ helper_scripts/MSRC-12-Kinect-Gesture/convert_TestData.py
@@ -12,7 +12,7 @@ output_folder = os.path.abspath('TestOutput')
 os.chdir('..' + sep + '..')
 os.chdir('src')
 
-exit_code = os.system('python main.py -c ' + config_file + ' -i ' + input_folder + ' -o ' + output_folder)
+exit_code = os.system('python main.py -c \"' + config_file + '\" -i \"' + input_folder + '\" -o \"' + output_folder + "\"")
 
 if(exit_code ==  0):
   print( colorama.Fore.GREEN )
