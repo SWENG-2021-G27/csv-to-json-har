45baaa37ee809656b8949c6857355eba8b7f79ae
Neimhin <nrobinso@tcd.ie>
2021-04-22 17:48:04 +0100

    consistency checkey is failing

--- helper_scripts/unit_test.py
+++ helper_scripts/unit_test.py
@@ -28,15 +28,22 @@ def test():
         with open(os.path.join('Verified',output)) as verified_file:
           print(dim("\tcomparing " + output))
           number_of_files_compared += 1
-          test = json.load(test_file)
-          verified = json.load(verified_file)
-          for key in test.keys():
-            if not test[key] == verified[key]:
-              differences += 1
-              print("\t\t" + red("For " + output + " test[" + key + "] is not the same as TestOutput[" + key + "]"))
-          if(differences != 0):
-            print('\t\t' + str(differences) + " difference" + plural(differences) + " between TestOutput/" + output + " and Verified/" + output)
-            total_differences += differences
+          try:
+            test = json.load(test_file)
+            verified = json.load(verified_file)
+            for key in test.keys():
+              if not key in verified:
+                differences += 1
+                print("\t\t" + red("For " + output + " verified[" + key + "] does not exist"))
+              elif not test[key] == verified[key]:
+                differences += 1
+                print("\t\t" + red("For " + output + " test[" + key + "] is not the same as verified[" + key + "]"))
+            if(differences != 0):
+              print('\t\t' + str(differences) + " difference" + plural(differences) + " between TestOutput/" + output + " and Verified/" + output)
+              total_differences += differences
+          except Exception as e:
+            print("Exception raised while comparing " + output + ": " + str(e))
+            total_differences += 1
 
 
   if(total_differences == 0):
